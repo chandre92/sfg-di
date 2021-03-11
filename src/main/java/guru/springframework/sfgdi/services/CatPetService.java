919993c31de4fe936f97ce441493e6b4a2fb0a15
chandre <disable1992@gmail.com>
2021-03-11 21:07:19 +0200

    di-assigment end

--- src/main/java/guru/springframework/sfgdi/services/CatPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/CatPetService.java
@@ -1,11 +1,13 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Service("cat")
+@Profile("cat")
+@Service
 public class CatPetService implements PetService {
     @Override
     public String getPetType() {
-        return "Cars Are the Best!";
+        return "Cats Are the Best!";
     }
 }
